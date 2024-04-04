93385b823a55402da2ebcb93e5e1c5f6d1ec3efa
Gustavo Kawamoto <kawamotus@academico.intra>
2024-04-04 19:34:46 -0300

    Select da área de clientes funcionando

--- services/ClienteService.js
+++ services/ClienteService.js
@@ -4,7 +4,11 @@ import cliente from "../models/cliente.js";
 const Cliente = mongoose.model("Cliente", cliente);
 
 class ClienteService{
+    SelectAll(){
+        return Cliente.find();
+    }
 
+    
 }
 
 export default new ClienteService();
